9adf103100e1365a9a77c311c97ea2fc709827e0
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-26 09:46:58 -0400

    connects mongoose

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,28 @@
 require("dotenv").config();
 // pull PORT from .env, give default value of 4000
 const { PORT = 3000 } = process.env;
+const MONGODB_URL = process.env.MONGODB_URL;
 // import express
 const express = require("express");
 // create application object
 const app = express();
+
+// import mongoose
+const mongoose = require("mongoose");
+
+///////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////////
+// Establish Connection
+mongoose.connect(MONGODB_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
+// Connection Events
+mongoose.connection
+    .on("open", () => console.log("You are connected to mongoose"))
+    .on("close", () => console.log("You are disconnected from mongoose"))
+    .on("error", (error) => console.log(error));
 
 ///////////////////////////////
 // ROUTES
