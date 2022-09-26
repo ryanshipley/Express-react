bf4f85486a109aef6b87f982382a48b90ebe4b13
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-26 11:15:05 -0400

    adds delete and edit

--- backend/server.js
+++ backend/server.js
@@ -69,6 +69,30 @@ app.get("/people", async (req, res) => {
     }
 });
 
+// PEOPLE DELETE ROUTE
+app.delete("/people/:id", async (req, res) => {
+    try {
+      // send all people
+      res.json(await People.findByIdAndRemove(req.params.id));
+    } catch (error) {
+      //send error
+      res.status(400).json(error);
+    }
+  });
+  
+  // PEOPLE UPDATE ROUTE
+  app.put("/people/:id", async (req, res) => {
+    try {
+      // send all people
+      res.json(
+        await People.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      );
+    } catch (error) {
+      //send error
+      res.status(400).json(error);
+    }
+  });
+
 // PEOPLE CREATE ROUTE
 app.post("/people", async (req, res) => {
     try {
